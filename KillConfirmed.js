e6a075cc816d69805fa9440a3a21f09bb6b262ed
MichaelPardoCadenas <145122584+MichaelPardoCadenas@users.noreply.github.com>
2024-12-05 15:43:41 +0100

    Add files via upload

---
+++ KillConfirmed.js
@@ -0,0 +1,18 @@
+
+let container = document.querySelector('.container');
+function SecondPage() {
+
+
+}
+
+functcondPage = document.createElement('div');
+
+    let gamePage = document.createElement('div');
+    gamePage.classList.add('game-page');
+    gamePage.style.backgroundImage = "./image/gamePage"
+    container.append(gamePage);
+
+
+
+}
+
